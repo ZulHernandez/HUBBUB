5abba49ebe44c883c167e940ae8aadea129d6809
ZulHernandez <saululises1912@gmail.com>
2022-06-02 17:36:23 -0500

    Hola
    New Pull

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css" integrity="sha512-/zs32ZEJh+/EO2N1b0PEdoA10JkdC3zJ8L5FTiQu82LR9S/rOQNfQN7U59U9BC12swNeRAz3HSzIL2vpp4fv3w==" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="src/css/general.css" rel="stylesheet">
+    <title>
+        HUBBUB | El ruido
+    </title>
+
+</head>
+
+<body>
+    <div class="container-fluid hero">
+        <div class="row" style="padding: 0px 60px;">
+            <div class="columna col-2"> </div>
+            <div class="columna col-2">
+                <img class="logoHero" src="assets/vector/HUBBUBLogo.svg">
+            </div>
+            <div class="columna col-1">
+                <center>
+                    <img src="assets/vector/hrVertical.svg">
+                </center>
+            </div>
+            <div class="columna col-5" style="background-color: red;"> </div>
+            <div class="columna col-2" style="background-color: blue;"> </div>
+        </div>
+        <div class="canvaClasRuido">
+            <canvas id="canvaClasRuido"></canvas>
+        </div>
+</body>
+<style type="text/css">
+
+</style>
+<script src="src/js/chart.js" type="text/javascript"></script>
+
+</html>
